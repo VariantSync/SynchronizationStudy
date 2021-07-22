7b0794e20b7998529568c36818bd41ee2b4d439d
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-07-22 13:52:45 +0200

    Added Maven structure and Main file

---
+++ src/main/java/de/variantsync/studies/sync/SynchronizationStudy.java
@@ -0,0 +1,11 @@
+package de.variantsync.studies.sync;
+
+import de.variantsync.evolution.util.Logger;
+
+public class SynchronizationStudy {
+    public static void main(String... args) {
+        System.out.println("Hello World!");
+        Logger.initConsoleLogger();
+        Logger.info("Hello World from the Logger!");
+    }
+}
