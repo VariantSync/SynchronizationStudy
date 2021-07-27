fbdc957adcc37b4fdf8df5afce3cd41a4227e9a8
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-07-27 16:31:35 +0000

    Working on diff parsing

--- src/main/java/de/variantsync/studies/sync/diff/DiffParser.java
+++ src/main/java/de/variantsync/studies/sync/diff/DiffParser.java
@@ -3,6 +3,7 @@ package de.variantsync.studies.sync.diff;
 import de.variantsync.evolution.util.NotImplementedException;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DiffParser {
@@ -16,12 +17,37 @@ public class DiffParser {
         if (lines.isEmpty()) {
             return new OriginalDiff(new ArrayList<>());
         }
+        List<FileDiff> fileDiffs = new LinkedList<>();
+        // Determine the substring which a FileDiff starts with
+        String fileDiffStart = "";
         if (lines.get(0).startsWith("diff")) {
-            // Several files were processed
+            // Several files were processed, the diff of each file starts with the 'diff' command that was used
+            fileDiffStart = "diff";
         } else if (lines.get(0).startsWith("---")) {
-            // Only one file was processed
+            // Only one file was processed, the diff of the file starts with the hunk header
+            fileDiffStart = "---";
         }
 
+        List<String> fileDiffContent = null;
+        for (String line : lines) {
+            if (line.startsWith(fileDiffStart)) {
+                // Create a FileDiff from the collected lines
+                if (fileDiffContent != null) {
+                    fileDiffs.add(parseFileDiff(fileDiffContent));
+                }
+                // Reset the lines that should go into the next FileDiff
+                fileDiffContent = new LinkedList<>();
+            }
+            if (fileDiffContent == null) {
+                throw new IllegalArgumentException("The provided lines do not contain one of the expected fileDiffStart values");
+            }
+            fileDiffContent.add(line);
+        }
+
+        return new OriginalDiff(fileDiffs);
+    }
+
+    private static FileDiff parseFileDiff(List<String> fileDiffContent) {
         throw new NotImplementedException();
     }
 }
