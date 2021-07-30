4e868af6d1c8ca66b5aa7fa8c9a792e0bea2c544
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-07-30 15:34:27 +0000

    Changed PCBasedFilter to now filter based on the target variant, not configurations

--- src/main/java/de/variantsync/studies/sync/SynchronizationStudy.java
+++ src/main/java/de/variantsync/studies/sync/SynchronizationStudy.java
@@ -1,11 +1,81 @@
 package de.variantsync.studies.sync;
 
-import de.variantsync.evolution.util.Logger;
+import de.ovgu.featureide.fm.core.base.IFeatureModel;
+import de.variantsync.evolution.feature.Sample;
+import de.variantsync.evolution.feature.Variant;
+import de.variantsync.evolution.io.Resources;
+import de.variantsync.evolution.util.CaseSensitivePath;
+import de.variantsync.evolution.util.NotImplementedException;
+import de.variantsync.evolution.util.list.NonEmptyList;
+import de.variantsync.evolution.variability.CommitPair;
+import de.variantsync.evolution.variability.SPLCommit;
+import de.variantsync.evolution.variability.VariabilityDataset;
+import de.variantsync.evolution.variability.pc.VariantGenerationOptions;
+import de.variantsync.studies.sync.error.SetupError;
+
+import java.nio.file.Path;
+import java.util.*;
 
 public class SynchronizationStudy {
+    // TODO: Set in external config
+    private static final Path datasetPath = Path.of("");
+    
     public static void main(String... args) {
-        System.out.println("Hello World!");
-        Logger.initConsoleLogger();
-        Logger.info("Hello World from the Logger!");
+        // Load VariabilityDataset
+        VariabilityDataset dataset;
+        try {
+            dataset = Resources.Instance().load(VariabilityDataset.class, datasetPath);
+        } catch (Resources.ResourceIOException e) {
+            throw new SetupError("Was not able to load dataset.");
+        }
+        
+        // Retrieve pairs/sequences of usable commits
+        Set<CommitPair<SPLCommit>> pairs = dataset.getCommitPairsForEvolutionStudy();
+        var history = dataset.getVariabilityHistory(setOfCommits -> {
+            List<NonEmptyList<SPLCommit>> sequences = new ArrayList<>(pairs.size());
+            pairs.stream().map(p -> {
+                List<SPLCommit> list = new ArrayList<>(2);
+                list.add(p.parent());
+                list.add(p.child());
+                return new NonEmptyList<>(list);
+            }).forEach(sequences::add);
+            return sequences;
+        });
+        
+        // For each pair
+        for (CommitPair<SPLCommit> pair : pairs) {
+            // Take next commit pair
+            SPLCommit oldCommit = pair.parent();
+            SPLCommit newCommit = pair.child();
+            // While more random configurations to consider
+            // Sample set of random configuration
+            List<Sample> configurations = sampleVariants(oldCommit.featureModel().run().get(), 0);
+            // Generate variants
+            CaseSensitivePath splDir = null;
+            CaseSensitivePath variantsDir = null;
+            oldCommit.presenceConditions().run().get().generateVariant(null, splDir, variantsDir, VariantGenerationOptions.ExitOnError);
+            newCommit.presenceConditions().run().get().generateVariant(null, splDir, variantsDir, VariantGenerationOptions.ExitOnError);
+            // Select next source variant
+            // copy source variant
+            // checkout previous revision of copied source variant
+            
+            // apply diff to source variant and its copy to receive patch
+            
+            // for each target variant
+            // select next target
+            // copy target variant
+            // checkout previous revision of copied target variant
+            // apply patch to copied target variant
+            // diff patch result and target variant
+            // evaluate diff
+            // evaluate patch rejects
+        }
+        
     }
+
+    public static List<Sample> sampleVariants(IFeatureModel featureModel, int n) {
+        throw new NotImplementedException();
+    }
+    
+    
 }
--- src/main/java/de/variantsync/studies/sync/diff/filter/PCBasedFilter.java
+++ src/main/java/de/variantsync/studies/sync/diff/filter/PCBasedFilter.java
@@ -1,8 +1,8 @@
 package de.variantsync.studies.sync.diff.filter;
 
+import de.variantsync.evolution.feature.Variant;
 import de.variantsync.evolution.util.CaseSensitivePath;
 import de.variantsync.evolution.util.Logger;
-import de.variantsync.evolution.variability.config.IConfiguration;
 import de.variantsync.evolution.variability.pc.Artefact;
 import de.variantsync.studies.sync.diff.components.FileDiff;
 import de.variantsync.studies.sync.error.Panic;
@@ -13,18 +13,16 @@ import java.nio.file.Path;
 public class PCBasedFilter implements IFileDiffFilter, ILineFilter{
     private final Artefact oldTraces;
     private final Artefact newTraces;
-    private final IConfiguration oldConfig;
-    private final IConfiguration newConfig;
+    private final Variant targetVariant;
     private final Path oldVersion;
     private final Path newVersion;
     
-    PCBasedFilter(IConfiguration oldConfig, IConfiguration newConfig, Artefact oldTraces, Artefact newTraces, Path oldVersionRoot, Path newVersionRoot) {
-        this.oldConfig = oldConfig;
-        this.newConfig = newConfig;
+    PCBasedFilter(Artefact oldTraces, Artefact newTraces, Variant targetVariant, Path oldVersionRoot, Path newVersionRoot) {
         this.oldTraces = oldTraces;
         this.newTraces = newTraces;
-        oldVersion = oldVersionRoot;
-        newVersion = newVersionRoot;
+        this.targetVariant = targetVariant;
+        this.oldVersion = oldVersionRoot;
+        this.newVersion = newVersionRoot;
     }
 
     @Override
@@ -36,9 +34,9 @@ public class PCBasedFilter implements IFileDiffFilter, ILineFilter{
     @Override
     public boolean shouldKeep(Path filePath, int index) {
         if (filePath.startsWith(oldVersion)) {
-            return shouldKeep(oldConfig, oldTraces, filePath, index);
+            return shouldKeep(targetVariant, oldTraces, filePath, index);
         } else if (filePath.startsWith(newVersion)) {
-            return shouldKeep(newConfig, newTraces, filePath, index);
+            return shouldKeep(targetVariant, newTraces, filePath, index);
         } else {
             String message = "The given path '" + filePath + "' does not match any of the versions' paths";
             Logger.error(message);
@@ -46,10 +44,10 @@ public class PCBasedFilter implements IFileDiffFilter, ILineFilter{
         }
     }
     
-    private static boolean shouldKeep(IConfiguration configuration, Artefact traces, Path filePath, int index) {
+    private static boolean shouldKeep(Variant targetVariant, Artefact traces, Path filePath, int index) {
         Node pc = traces
                 .getPresenceConditionOf(new CaseSensitivePath(filePath), index)
                 .expect("Was not able to load PC for line " + index + " of " + filePath);
-        return configuration.satisfies(pc);
+        return targetVariant.isImplementing(pc);
     }
 }
