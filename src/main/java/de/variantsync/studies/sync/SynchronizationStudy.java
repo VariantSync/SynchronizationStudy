7bc27f778117d2c8fc302567d3480028d75f1b6b
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-08-02 11:08:01 +0000

    Debugging experimental framework

--- src/main/java/de/variantsync/studies/sync/SynchronizationStudy.java
+++ src/main/java/de/variantsync/studies/sync/SynchronizationStudy.java
@@ -6,6 +6,8 @@ import de.variantsync.evolution.feature.Sample;
 import de.variantsync.evolution.feature.Variant;
 import de.variantsync.evolution.feature.sampling.FeatureIDESampler;
 import de.variantsync.evolution.io.Resources;
+import de.variantsync.evolution.io.data.VariabilityDatasetLoader;
+import de.variantsync.evolution.repository.Branch;
 import de.variantsync.evolution.repository.SPLRepository;
 import de.variantsync.evolution.util.CaseSensitivePath;
 import de.variantsync.evolution.util.Logger;
@@ -21,10 +23,7 @@ import de.variantsync.studies.sync.diff.filter.PCBasedFilter;
 import de.variantsync.studies.sync.diff.splitting.DefaultContextProvider;
 import de.variantsync.studies.sync.diff.splitting.DiffSplitter;
 import de.variantsync.studies.sync.error.Panic;
-import de.variantsync.studies.sync.shell.CpCommand;
-import de.variantsync.studies.sync.shell.DiffCommand;
-import de.variantsync.studies.sync.shell.PatchCommand;
-import de.variantsync.studies.sync.shell.ShellExecutor;
+import de.variantsync.studies.sync.shell.*;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.jetbrains.annotations.NotNull;
 import org.prop4j.Node;
@@ -33,35 +32,54 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class SynchronizationStudy {
     // TODO: Set in external config
-    private static final Path datasetPath = Path.of("");
-    private static final Path workDir = Path.of("");
+    private static final Path datasetPath = Path.of("/home/alex/data/synchronization-study/variability-busybox");
+    private static final Path workDir = Path.of("/home/alex/data/synchronization-study/workdir");
     private static final int randomRepeats = 1;
-    private static final SPLRepository splRepositoryV0 = new SPLRepository(null);
-    private static final SPLRepository splRepositoryV1 = new SPLRepository(null);
+    private static final int numVariants = 3;
+    private static final SPLRepository splRepositoryV0 = new SPLRepository(workDir.getParent().resolve("busybox-V0"));
+    private static final SPLRepository splRepositoryV1 = new SPLRepository(workDir.getParent().resolve("busybox-V1"));
+    private static final String branchName = "master";
     private static final CaseSensitivePath variantsDirV0 = new CaseSensitivePath(workDir.resolve("V0Variants"));
     private static final CaseSensitivePath variantsDirV1 = new CaseSensitivePath(workDir.resolve("V1Variants"));
     private static final Path patchDir = workDir.resolve("TARGET");
     private static final Path normalPatchFile = workDir.resolve("patch.txt");
     private static final Path pcBasedPatchFile = workDir.resolve("pc-patch.txt");
-    private static final FeatureIDESampler variantSampler = FeatureIDESampler.CreateRandomSampler(randomRepeats);
+    private static final FeatureIDESampler variantSampler = FeatureIDESampler.CreateRandomSampler(numVariants);
     private static final ShellExecutor shell = new ShellExecutor(Logger::info, Logger::error, workDir);
 
 
     public static void main(String... args) {
+        // Initialize the library
+        de.variantsync.evolution.Main.Initialize();
+
+//        Logger.status("Initializing SPL repository");
+//        try {
+//            splRepositoryV0.checkoutBranch(new Branch(branchName));
+//            splRepositoryV0.checkoutBranch(new Branch(branchName));
+//        } catch (GitAPIException | IOException e) {
+//            panic("Was not able to checkout branch " + branchName + " for SPL repo.", e);
+//        }
+
         // Load VariabilityDataset
+        Logger.status("Loading variability dataset.");
         VariabilityDataset dataset = null;
         try {
-            dataset = Resources.Instance().load(VariabilityDataset.class, datasetPath);
+            Resources instance = Resources.Instance();
+            final VariabilityDatasetLoader datasetLoader = new VariabilityDatasetLoader();
+            instance.registerLoader(VariabilityDataset.class, datasetLoader);
+            dataset = instance.load(VariabilityDataset.class, datasetPath);
         } catch (Resources.ResourceIOException e) {
-            panic("Was not able to load dataset.");
+            panic("Was not able to load dataset.", e);
         }
 
         // Retrieve pairs/sequences of usable commits
-        Set<CommitPair<SPLCommit>> pairs = dataset.getCommitPairsForEvolutionStudy();
+        Logger.status("Retrieving commit pairs");
+        Set<CommitPair<SPLCommit>> pairs = Objects.requireNonNull(dataset).getCommitPairsForEvolutionStudy();
 //        var history = dataset.getVariabilityHistory(setOfCommits -> {
 //            List<NonEmptyList<SPLCommit>> sequences = new ArrayList<>(pairs.size());
 //            pairs.stream().map(p -> {
@@ -73,13 +91,16 @@ public class SynchronizationStudy {
 //            return sequences;
 //        });
 
-        // TODO: Handle empty diff between source versions
         // For each pair
+        Logger.status("Starting diffing and patching...");
         for (CommitPair<SPLCommit> pair : pairs) {
             // Take next commit pair
             SPLCommit commitV0 = pair.parent();
             SPLCommit commitV1 = pair.child();
 
+            Logger.info("Next V0 commit: " + commitV0);
+            Logger.info("Next V1 commit: " + commitV1);
+            Logger.info("Checkout of commits in SPL repo...");
             // Checkout the commits in the SPL repository
             try {
                 splRepositoryV0.checkoutCommit(commitV0);
@@ -87,19 +108,32 @@ public class SynchronizationStudy {
             } catch (GitAPIException | IOException e) {
                 panic("Was not able to checkout commit for SPL repository.", e);
             }
+            Logger.info("Done.");
 
             IFeatureModel modelV0 = commitV0.featureModel().run().orElseThrow();
+            var test = modelV0.getFeature("CONFIG_BUILD_LIBBUSYBOX");
             IFeatureModel modelV1 = commitV1.featureModel().run().orElseThrow();
+            var test2 = modelV1.getFeature("CONFIG_BUILD_LIBBUSYBOX");
             final Node featureModelFormulaV0 = new FeatureModelFormula(modelV0).getPropositionalNode();
             final Node featureModelFormulaV1 = new FeatureModelFormula(modelV0).getPropositionalNode();
 
             // While more random configurations to consider
             for (int i = 0; i < randomRepeats; i++) {
+                Logger.status("Starting repetition " + (i+1) + " of " + randomRepeats + " with (random) variants.");
                 // Sample set of random variants
+                Logger.status("Sampling next set of variants...");
                 Sample configurations = variantSampler.sample(modelV0);
-
+                Logger.status("Done. Sampled " + configurations.variants().size() + " variants.");
+                
+                Logger.status("Cleaning variants dir V0.");
+                shell.execute(new RmCommand(variantsDirV0.path()).recursive());
+                Logger.status("Cleaning variants dir V1.");
+                shell.execute(new RmCommand(variantsDirV1.path()).recursive());
+                
                 // Generate the randomly selected variants at both versions
+                Logger.status("Generating variants...");
                 for (Variant variant : configurations.variants()) {
+                    Logger.status("Generating variant " + variant.getName());
                     // TODO: Check whether this is enough. It only checks satisfiability, but might not check whether the variables exist in the feature model. Maybe check whether the set of selected variables exists?
                     if (!variant.isImplementing(featureModelFormulaV0)) {
                         panic("Sampled " + variant + " is not valid for feature model " + modelV0 + "!");
@@ -110,75 +144,103 @@ public class SynchronizationStudy {
                     commitV0.presenceConditions().run().orElseThrow().generateVariant(variant, new CaseSensitivePath(splRepositoryV0.getPath()), variantsDirV0.resolve(variant.getName()), VariantGenerationOptions.ExitOnError);
                     commitV1.presenceConditions().run().orElseThrow().generateVariant(variant, new CaseSensitivePath(splRepositoryV1.getPath()), variantsDirV1.resolve(variant.getName()), VariantGenerationOptions.ExitOnError);
                 }
+                Logger.status("Done.");
 
                 // Select next source variant
                 for (Variant source : configurations.variants()) {
-                    // apply diff to both versions of source variant
+                    Logger.status("Starting diff application for source variant " + source.getName());
+                    // Apply diff to both versions of source variant
+                    Logger.info("Diffing source...");
                     OriginalDiff originalDiff = getOriginalDiff(variantsDirV0.path().resolve(source.getName()), variantsDirV1.path().resolve(source.getName()));
-                    FineDiff fineDiff = getFineDiff(originalDiff);
-
-                    // Save the fine Diff
-                    try {
-                        Files.write(normalPatchFile, fineDiff.toLines());
-                    } catch (IOException e) {
-                        panic("Was not able to save FineDiff to file " + normalPatchFile);
+                    if (originalDiff.isEmpty()) {
+                        // There was no change to this variant, so we can skip it as source
+                        continue;
                     }
+                    Logger.info("Converting diff...");
+                    // Convert the original diff into a fine diff
+                    FineDiff fineDiff = getFineDiff(originalDiff);
+                    saveDiff(fineDiff, normalPatchFile);
+                    Logger.info("Saved fine diff.");
 
-                    // for each target variant
+                    // For each target variant,
+                    Logger.status("Starting patch application for source variant " + source.getName());
                     for (Variant target : configurations.variants()) {
+                        Logger.info("Considering variant " + target.getName() + " as next target.");
                         if (target == source) {
                             continue;
                         }
-                        // TODO: Extract creation of diff, saving of diff, creation of copy, application of patch, evaluation of result, and clean-up
-                        // copy target variant
-                        shell.execute(new CpCommand(variantsDirV0.path().resolve(target.getName()), patchDir).recursive()).expect("Was not able to copy variant " + target.getName());
+                        Path pathToTarget = variantsDirV0.path().resolve(target.getName());
 
                         /* Application of patches without knowledge about features */
-                        // apply patch to copied target variant
-                        shell.execute(PatchCommand.Recommended(normalPatchFile), patchDir);
-                        // diff patch result and target variant
-                        {
-                            DiffCommand diffCommand = DiffCommand.Recommended(variantsDirV0.resolve(source.getName()).path(), variantsDirV1.resolve(source.getName()).path());
-                            List<String> output = shell.execute(diffCommand).expect("Was not able to diff variants.");
-                            // evaluate diff
-                            if (output.isEmpty()) {
-                                // TODO
-                                Logger.info("No difference between patched variant and actual target");
-                            } else {
-                                // TODO
-                                Logger.warning("There are differences between the patched variant and the actual target");
-                            }
-                        }
-                        // evaluate patch rejects
-                        evaluatePatchRejects();
+                        Logger.info("Applying patch without knowledge about features...");
+                        // Apply the fine diff to the target variant
+                        applyPatch(normalPatchFile, pathToTarget);
+                        // Evaluate the patch result
+                        evaluatePatchResult(pathToTarget);
                         
+                        // TODO: Extract creation of diff, saving of diff, creation of copy, application of patch, evaluation of result, and clean-up
+
                         /* Application of patches with knowledge about features */
-                        // Clean patch directory
-                        // TODO: 
+                        Logger.info("Applying patch with knowledge about features...");
                         // Create target variant specific patch that respects PCs
                         FineDiff pcBasedDiff = getPCBasedDiff(originalDiff, commitV0, commitV1, target);
-                        // Save the fine Diff
-                        try {
-                            Files.write(pcBasedPatchFile, pcBasedDiff.toLines());
-                        } catch (IOException e) {
-                            panic("Was not able to save pc-based diff to file " + pcBasedPatchFile);
-                        }
-                        // copy target variant
-                        shell.execute(new CpCommand(variantsDirV0.path().resolve(target.getName()), patchDir).recursive()).expect("Was not able to copy variant " + target.getName());
-                        // apply patch to copied target variant
-                        shell.execute(PatchCommand.Recommended(pcBasedPatchFile), patchDir);
-                        // diff patch result and target variant
-                        // evaluate diff
-                        // evaluate patch rejects
+                        saveDiff(pcBasedDiff, pcBasedPatchFile);
+                        // Apply the patch
+                        applyPatch(pcBasedPatchFile, pathToTarget);
+                        // Evaluate the result
+                        evaluatePatchResult(pathToTarget);
+                        
+                        Logger.info("Finished patching for source " + source.getName() + " and target " + target.getName());
                     }
                 }
             }
         }
     }
-    
+
     private static void evaluatePatchRejects() {
         // TODO: Implement evaluation
         Logger.error("TODO");
+    }
+
+    private static void saveDiff(FineDiff fineDiff, Path file) {
+        // Save the fine diff to a file
+        try {
+            Files.write(file, fineDiff.toLines());
+        } catch (IOException e) {
+            panic("Was not able to save diff to file " + file);
+        }
+    }
+
+    private static void applyPatch(Path patchFile, Path targetVariant) {
+        // Clean patch directory
+        shell.execute(new RmCommand("./*").recursive(), patchDir);
+
+        // copy target variant
+        shell.execute(new CpCommand(targetVariant, patchDir).recursive()).expect("Was not able to copy variant " + targetVariant);
+
+        /* Application of patches without knowledge about features */
+        // apply patch to copied target variant
+        shell.execute(PatchCommand.Recommended(patchFile), patchDir);
+
+    }
+
+    private static void evaluatePatchResult(Path targetVariant) {
+        // diff patch result and target variant
+        DiffCommand diffCommand = DiffCommand.Recommended(targetVariant, patchDir);
+        List<String> output = shell.execute(diffCommand).expect("Was not able to diff variants.");
+        // evaluate diff
+        if (output.isEmpty()) {
+            // TODO
+            Logger.info("No difference between patched variant and actual target");
+        } else {
+            // TODO
+            Logger.warning("There are differences between the patched variant and the actual target");
+        }
+        
+        // evaluate patch rejects
+        evaluatePatchRejects();
+        
+        // TODO: cleanup rejects etc.
     }
 
     @NotNull
@@ -186,7 +248,7 @@ public class SynchronizationStudy {
         DefaultContextProvider contextProvider = new DefaultContextProvider(workDir);
         return DiffSplitter.split(originalDiff, contextProvider);
     }
-    
+
     private static FineDiff getPCBasedDiff(OriginalDiff originalDiff, SPLCommit commitV0, SPLCommit commitV1, Variant target) {
         Artefact tracesV0 = commitV0.presenceConditions().run().orElseThrow();
         Artefact tracesV1 = commitV1.presenceConditions().run().orElseThrow();
@@ -195,7 +257,7 @@ public class SynchronizationStudy {
         DefaultContextProvider contextProvider = new DefaultContextProvider(workDir);
         return DiffSplitter.split(originalDiff, pcBasedFilter, pcBasedFilter, contextProvider);
     }
-    
+
     private static OriginalDiff getOriginalDiff(Path v0Path, Path v1Path) {
         DiffCommand diffCommand = DiffCommand.Recommended(v0Path, v1Path);
         List<String> output = shell.execute(diffCommand).expect("Was not able to diff variants.");
