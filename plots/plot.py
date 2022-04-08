ec8b7c078cf4b22c0b87c5913af190502b531d19
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2022-04-08 21:08:53 +0200

    Updated plotting

--- plots/plot.py
+++ plots/plot.py
@@ -57,7 +57,7 @@ def rq1_piechart(failure, success, outputPath):
 
 
 def rq1_barchart(experiment, outputPath):
-    colourscheme = 'forestgreen', 'darkorange'
+    # colourscheme = 'teal', 'orange'
     commit_success = experiment.commitSuccess
     commit_failed = experiment.getNumCommitFailures()
     file_success = experiment.fileSuccess
@@ -85,20 +85,20 @@ def rq1_barchart(experiment, outputPath):
     fig, ax = plt.subplots()
     success_percentage = numpy.divide(success_vals, sum_vals)
     failed_percentage = numpy.divide(failed_vals, sum_vals)
-    success_rects = ax.bar(x - 0.05 - widthOfBars / 2, 100*success_percentage, widthOfBars, label='applicable', color=colourscheme[0])
-    failed_rects = ax.bar(x + 0.05 + widthOfBars / 2, 100*failed_percentage, widthOfBars, label='failed', color=colourscheme[1])
+    success_rects = ax.bar(x - 0.05 - widthOfBars / 2, 100*success_percentage, widthOfBars, label='applicable', alpha=1)
+    failed_rects = ax.bar(x + 0.05 + widthOfBars / 2, 100*failed_percentage, widthOfBars, label='failed', alpha=1)
 
-    def label_values(percentage, absolute, offset, c):
+    def label_values(percentage, absolute, offset):
         for i, v in enumerate(percentage):
             v = 100*v
-            label = str(numpy.round(v, 2))
+            label = "{:2.1f}".format(v)
             label += "%"
-            ax.text(i+offset+0.1, v + 5, label, color=c, fontweight='bold')
+            ax.text(i+offset+0.1, v + 5, label)
             label = "({:,.0f})".format(absolute[i])
-            ax.text(i+offset, v + 1, label, color=c, fontweight='bold')
+            ax.text(i+offset, v + 1, label)
 
-    label_values(success_percentage, success_vals, -0.4, colourscheme[0])
-    label_values(failed_percentage, failed_vals, -0.05, colourscheme[1])
+    label_values(success_percentage, success_vals, -0.4)
+    label_values(failed_percentage, failed_vals, -0.05)
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Percentage of patches')
@@ -122,8 +122,9 @@ def rq1_barchart(experiment, outputPath):
     plt.savefig(outputPath, dpi=DPI, bbox_inches='tight')
 
 
-def correctness_barchart(experiment, outputPath):
-    colourscheme = 'forestgreen', 'darkorange'
+def correctness_barchart(experiment, outputPath, colourscheme):
+    # colourscheme = 'forestgreen', 'darkorange'
+    colors = [colourscheme.tp, colourscheme.fp, colourscheme.fn_wronglocation, colourscheme.tn, colourscheme.fn_missing]
 
     labels = ['correct\n(TP)', 'invalid\n(FP)', 'wrong location\n(FN)', 'not required\n(TN)', 'missing\n(FN)']
     success_vals = [experiment.tp, experiment.fp, experiment.wrongLocation]
@@ -139,21 +140,28 @@ def correctness_barchart(experiment, outputPath):
     fig, ax = plt.subplots()
     success_percentage = 100 * numpy.divide(success_vals, numpy.sum(success_vals))
     failed_percentage = 100 * numpy.divide(failed_vals, numpy.sum(failed_vals))
-    success_rects = ax.bar(s, success_percentage, widthOfBars, label='applicable', color=colourscheme[0])
-    failed_rects = ax.bar(f, failed_percentage, widthOfBars, label='failed', color=colourscheme[1])
+    for i,x in enumerate(s):
+        success_rects = ax.bar(x, success_percentage[i], widthOfBars, color=colors[i], edgecolor='black')
+    for i,x in enumerate(f):
+        failed_rects = ax.bar(x, failed_percentage[i], widthOfBars, color=colors[i+3], edgecolor='black')
     ax.vlines(2.65, ymin=0, ymax=105, color='black')
 
-    def label_values(percentage, absolute, offset, c):
+    def label_values(percentage, absolute, offset):
         for i, v in enumerate(percentage):
             v = v
-            label = str(numpy.round(v, 2))
+            label = "{:2.1f}".format(v)
             label += "%"
-            ax.text(i+offset+0.2, v + 5, label, color=c, fontweight='bold')
+            ax.text(i+offset+0.3, v + 5, label)
             label = "({:,.0f})".format(absolute[i])
-            ax.text(i+offset, v + 1, label, color=c, fontweight='bold')
+            ax.text(i+offset, v + 1, label)
 
-    label_values(success_percentage, success_vals, -0.42, colourscheme[0])
-    label_values(failed_percentage, failed_vals, 2.72, colourscheme[1])
+    label_values(success_percentage, success_vals, -0.42)
+    label_values(failed_percentage, failed_vals, 2.8)
+
+    props = dict(boxstyle='round', alpha=0.2)
+    ax.text(1.9, 95, " applied\npatches", bbox=props)
+    props = dict(boxstyle='round', alpha=0.2)
+    ax.text(4.5, 95, "  failed\npatches", bbox=props)
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Percentage of patches')
@@ -163,14 +171,13 @@ def correctness_barchart(experiment, outputPath):
     # ax.set_title('Scores by group and gender')
     ax.set_xticks(numpy.append(s, f))
     ax.set_xticklabels(labels)
-    ax.legend(loc=1)
 
-    ax.bar_label(success_rects,
-                 labels=map(lambda x: "", success_vals),
-                 padding=3)
-    ax.bar_label(failed_rects,
-                 labels=map(lambda x: "", failed_vals),
-                 padding=3)
+    # ax.bar_label(success_rects,
+    #              labels=map(lambda x: "", success_vals),
+    #              padding=3)
+    # ax.bar_label(failed_rects,
+    #              labels=map(lambda x: "", failed_vals),
+    #              padding=3)
 
     fig.tight_layout()
 
@@ -244,27 +251,38 @@ def rq3_barchart(experiment, colourscheme, outDir):
     # print(numpy.sum(fvals))
 
     # normalize values
-    # def normalize(vals, total):
-    #     return list(map(lambda x: float(x)/float(total), vals))
-    # nvals = normalize(nvals, ntotal)
-    # fvals = normalize(fvals, ftotal)
+    def normalize(vals, total):
+        return list(map(lambda x: float(x)/float(total), vals))
+    nvals = numpy.multiply(normalize(nvals, ntotal), 100)
+    fvals = numpy.multiply(normalize(fvals, ftotal), 100)
 
-    _scalefactor = 1
-    x = numpy.arange(_scalefactor*len(labels), step = _scalefactor)
+    _scalefactor = 3
+    start=2
+    stop=start+_scalefactor * len(labels)
+    x = numpy.arange(start=start, stop=stop, step=_scalefactor)
     # print(x)
-    widthOfBars = 0.35
+    widthOfBars = 0.5
 
     fig, ax = plt.subplots()
-    nrects = ax.bar(x - widthOfBars/2, nvals, widthOfBars, label='without domain knowledge')
-    frects = ax.bar(x + widthOfBars/2, fvals, widthOfBars, label='with domain knowledge')
+    nrects = ax.bar(x - 0.05 - widthOfBars/2, nvals, widthOfBars, label='without domain knowledge')
+    frects = ax.bar(x + 0.05 + widthOfBars/2, fvals, widthOfBars, label='with domain knowledge')
     
     # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Number of Patches')
-    # ax.set_yscale('log')
-    # ax.set_title('Scores by group and gender')
+    ax.set_ylabel('Percentage of Patches')
+    ax.set_ylim([0, 100])
+    ax.set_xlim([-0.1, stop + 0.75 - _scalefactor/2])
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
     ax.legend()
+
+    def label_values(percentage, offset_x, offset_y):
+        for i, v in enumerate(percentage):
+            label = "{:2.1f}".format(v)
+            label += "%"
+            ax.text(i*_scalefactor + start + offset_x, v + offset_y, label)
+
+    label_values(nvals, -1.9, 1)
+    label_values(fvals, 0.5, 1)
 
     ax.bar_label(nrects,
         labels=map(lambda x: "", nvals),
@@ -370,7 +388,7 @@ def rq1(patchstrategy, outDir):
 
 def rq2(patchstrategy, colourscheme, outDir):
     print("RQ2")
-    correctness_barchart(patchstrategy, os.path.join(outDir, patchstrategy.name + "_correctness" + OUTPUT_FORMAT))
+    correctness_barchart(patchstrategy, os.path.join(outDir, patchstrategy.name + "_correctness" + OUTPUT_FORMAT), colourscheme)
 
 
 def rq3(experiment, colourscheme, outDir):
